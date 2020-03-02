bae9d82df9296ff80f9e8eff5dd4bccc00c9d5e5
K-ai30 <artpirate11@aim.com>
2020-03-01 21:10:09 -0500

    updated connection info

---
+++ db/trackerDBConnection.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Port; if not 3306
+    port: process.env.PORT,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  });
+
+  
+
+
+  module.exports = connection;
